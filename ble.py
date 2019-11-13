dab3018cbe5231aa65f18f962c8d3183a554e2ef
Alex Chi <iskyzh@gmail.com>
2019-11-13 19:55:26 +0800

    add ble for new push service

---
+++ ble.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import logging
+import asyncio
+import platform
+from loguru import logger
+from bleak import BleakClient
+from config import BLE_CHARACTERISTIC_UUID, BLE_ADDR
+
+async def ble_service(loop: asyncio.AbstractEventLoop, 
+                      msg_queue: asyncio.Queue):
+
+    async def put_to_queue(data):
+        await msg_queue.put(data)
+
+    def notification_handler(sender, data):
+        loop.create_task(put_to_queue(data))
+
+    logger.info("scanning client...")
+
+    client = BleakClient(BLE_ADDR, loop=loop)
+    logger.info("connecting to device {0} ...".format(BLE_ADDR))
+    await client.connect()
+    x = await client.is_connected()
+    logger.info("connected: {0}".format(x))
+
+    await client.start_notify(BLE_CHARACTERISTIC_UUID, notification_handler)
+
+    logger.info("notification registered")
+
+    disconnected_event = asyncio.Event()
+
+    def disconnect_callback(client):
+        loop.call_soon_threadsafe(disconnected_event.set)
+
+    client.set_disconnected_callback(disconnect_callback)
+
+    try:
+        await disconnected_event.wait()
+    except:
+        await client.stop_notify(BLE_CHARACTERISTIC_UUID, notification_handler)
+    
+    logger.info("disconnected from device")
+
+    await msg_queue.put(None)
+
+    await asyncio.sleep(0.5)
--- sync.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rsync -azP . pi@192.168.0.133:~/App/BlueMarine/ --exclude=node_modules/
